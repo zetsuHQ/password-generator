1ed48f83d6259849f880714b38aad4d0244b6630
zetsuHQ <chinforinfola@asd.com>
2023-05-13 02:15:17 -0300

    Syntax bug fixed.

--- password_generator.js
+++ password_generator.js
@@ -2,8 +2,8 @@ function password_generator(char_amount) {
     let new_password = ""
     char_types = {
         special_chars: ['!', '@', '#', '$', '%', '&', '*'],
-        letters:['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-        numbers:['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+        numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     }
 
     const generates_random_num = range => {
@@ -11,24 +11,24 @@ function password_generator(char_amount) {
     }
     // Verifies if the parameter is a number.
     if (typeof char_amount !== 'number') {
-        console.log("Insira um valor numérico.")
-    } else {
         const choose_char_type = () => {
             generates_random_num(5)
 
             switch (choose_char_type) {
-                case 1: 
+                case 1:
                     return 'special_chars'
-                case 2: case 3: case 4: 
+                case 2: case 3: case 4:
                     return 'letters'
-                case 5: 
+                case 5:
                     return 'numbers'
             }
             while (new_password.length < char_amount) {
 
             }
-
         }
+
+    } else {
+        console.log("Insira um valor numérico.")
     }
 }
 
