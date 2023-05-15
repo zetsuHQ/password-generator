7e00da028985af1d14593da54793f2a648507e5a
Gabriel Lima <gabrielunderlinezetsu@gmail.com>
2023-05-15 06:19:06 -0300

    Managed to link the script and HTML files

--- password_generator.js
+++ password_generator.js
@@ -1,5 +1,24 @@
-function password_generator(char_amount) {
-
+const chars = {
+    letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    specials: ['!', '@', '#', '$', '%', '&']
 }
 
-console.log(password_generator(7))
+const is_checked = (input) => {
+    if (input === "on") {
+        return true
+    } else {
+        return false
+    }
+}
+
+const generates_random_num = (range) => {
+    return Math.floor(Math.random() * range + 1)
+}
+
+function exec() {
+    console.log("funcionou")
+}
+
+const generate = document.getElementById('generate')
+
