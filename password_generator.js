1667afccbc2bd8ec5b5c11ce8884d9f624c39ec2
zetsu <gabrielunderlinezetsu@gmail.com>
2023-06-17 19:08:35 -0300

    Trying to rewrite script again

--- password_generator.js
+++ password_generator.js
@@ -4,45 +4,62 @@ const chars = {
     specials: ['!', '@', '#', '$', '%', '&']
 }
 
-function generates_rand(min = 0, max) {
+// Includes 'min', but doesn't include 'max' on possible results.
+function generates_rand(max, min = 0) {
     return Math.floor(Math.random() * max + min)
 }
 
 
-let current_parameters = {}
+// let current_parameters = {}
 
-function get_parameters() {
+// function get_parameters() {
+//     if (document.getElementById("contains-letters").checked) {
+//         current_parameters = { ...current_parameters, letters: chars.letters}
+//     }
+//     if (document.getElementById("contains-numbers").checked) {
+//         current_parameters = { ...current_parameters, numbers: chars.numbers}
+//     }
+//     if (document.getElementById("contains-specials").checked) {
+//         current_parameters = { ...current_parameters, specials: chars.specials}
+//     }
+// }
+
+// let char_amount = document.getElementById("length").value
+
+// function password_generator(){
+//     let new_password = ""
+//     for (let i = 0; i < char_amount; i++) {
+//         let new_char_type = current_parameters[generates_rand(Object.keys(current_parameters).length)]
+//         let new_char = new_char_type[generates_rand(new_char_type.length)]
+//         new_password += new_char
+//     }
+//     return new_password
+// }
+
+let parameters = {}
+
+function parser(parameters) {
     if (document.getElementById("contains-letters").checked) {
-        current_parameters = { ...current_parameters, letters: chars.letters}
-    }
-    if (document.getElementById("contains-numbers").checked) {
-        current_parameters = { ...current_parameters, numbers: chars.numbers}
-    }
-    if (document.getElementById("contains-specials").checked) {
-        current_parameters = { ...current_parameters, specials: chars.specials}
+        parameters.letters = chars[letters]
     }
 }
 
-let char_amount = document.getElementById("char-amount").value
-
-function password_generator(){
+function password_generator (parameters) {
     let new_password = ""
-    for (let i = 0; i < char_amount; i++) {
-        let new_char_type = current_parameters[generates_rand(Object.keys(current_parameters).length)]
-        let new_char = new_char_type[generates_rand(new_char_type.length)]
-        new_password += new_char
+    let new_char_type
+    let new_char = ""
+    for (i = 0; i < parameters.length; i++) {
+        new_char_type = chars[generates_rand]
     }
-    return new_password
 }
 
 let generate = document.getElementById("generate")
 
+let output = document.getElementById("actual-output")
+
 function exec() {
-    const output = document.getElementById("output")
-    if (output.style.display === "none") {
-        output.style.display = "block"
-    } else {
-        output.style.display = "none"
-    }
+    parser(parameters)
+    console.log(parameters.letters)
 }
 
+generate.addEventListener('click', exec)
