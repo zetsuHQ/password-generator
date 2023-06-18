ef3dd945ba8f39c161fc421888c863ae51dc5e23
zetsuHQ <chinforinfola@asd.com>
2023-06-18 18:18:51 -0300

    Now I can properly validate and parse the parameters.

--- password_generator.js
+++ password_generator.js
@@ -1,54 +1,54 @@
-const chars = {
-    letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-    numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-    specials: ['!', '@', '#', '$', '%', '&']
-}
+const chars = [
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    ['!', '@', '#', '$', '%', '&']
+]
 
 // Includes 'min', but doesn't include 'max' on possible results.
 function generates_rand(max, min = 0) {
     return Math.floor(Math.random() * max + min)
 }
 
-
-// let current_parameters = {}
-
-// function get_parameters() {
-//     if (document.getElementById("contains-letters").checked) {
-//         current_parameters = { ...current_parameters, letters: chars.letters}
-//     }
-//     if (document.getElementById("contains-numbers").checked) {
-//         current_parameters = { ...current_parameters, numbers: chars.numbers}
-//     }
-//     if (document.getElementById("contains-specials").checked) {
-//         current_parameters = { ...current_parameters, specials: chars.specials}
-//     }
-// }
-
-// let char_amount = document.getElementById("length").value
-
-// function password_generator(){
-//     let new_password = ""
-//     for (let i = 0; i < char_amount; i++) {
-//         let new_char_type = current_parameters[generates_rand(Object.keys(current_parameters).length)]
-//         let new_char = new_char_type[generates_rand(new_char_type.length)]
-//         new_password += new_char
-//     }
-//     return new_password
-// }
-
-let parameters = {}
-
-function parser(parameters) {
-    if (document.getElementById("contains-letters").checked) {
-        parameters.letters = chars[letters]
-    }
+function random_index(array) {
+    return generates_rand(array.length)
 }
 
-function password_generator (parameters) {
+function validate(parameters) {
+    const length = parameters[0]
+    if (length === "") {
+        window.alert("Please select your password's length.")
+        return false
+    } else if (parameters.length < 2) {
+        window.alert("Please select at least one character type.")
+        return false
+    } else return true
+}
+
+// Gets the value of each input, being
+function parser(parameters) {
+    const length = document.getElementById("length").value
+    parameters.push(document.getElementById("length").value)
+    if (document.getElementById("contains-letters").checked) {
+        parameters.push(chars[0])
+    }
+    if (document.getElementById("contains-caps").checked) {
+        parameters.push(chars[1])
+    }
+    if (document.getElementById("contains-numbers").checked) {
+        parameters.push(chars[2])
+    }
+    if (document.getElementById("contains-specials").checked) {
+        parameters.push(chars[3])
+    }
+
+}
+
+function password_generator(parameters) {
     let new_password = ""
     let new_char_type
     let new_char = ""
-    for (i = 0; i < parameters.length; i++) {
+    for (i = 0; i < parameters[0]; i++) {
         new_char_type = chars[generates_rand]
     }
 }
@@ -58,8 +58,15 @@ let generate = document.getElementById("generate")
 let output = document.getElementById("actual-output")
 
 function exec() {
-    parser(parameters)
-    console.log(parameters.letters)
+    exec: {
+        const parameters = []
+        parser(parameters)
+        const are_valid = validate(parameters)
+        if (!are_valid) {
+            break exec
+        }
+        console.log(parameters)
+    }
 }
 
 generate.addEventListener('click', exec)
